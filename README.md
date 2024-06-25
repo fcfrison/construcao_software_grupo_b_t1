2109e1cc24bc80a02231b21cbeca1e43001c4d74
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-06-25 17:52:19 +0000

    deploy: insere aquivo terraform.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Como rodar o programa em um container docker
 Rode os seguintes comandos:
-1. `docker build -t constStwApp:latest .`
-3. `docker run -d --rm -p 8080:8080 constStwApp`
+1. `docker build -t const_softw:latest .`
+2. `docker run -d --restart always --name const_soft_app -p 8080:8080 const_softw`
+
+# Para subir a infra na AWS a partir do Teraform
+- criar no diretório 'home' o arquivo '/home/.aws/credentials' e inserir as credenciais de acesso.
+OBS.: se estiver rodando no Codespaces, então o diretório '.aws' deverá ser inserido no '/home/codespaces'.
---
+++ infra/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "existing_ami" {
+  owners = ["self"]
+  filter {
+    name   = "image-id"
+    values = ["ami-0865cda3ee8358d58"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.existing_ami.id # Imagem de instancia com app em spring boot
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.allow_8080.id]
+
+  tags = {
+    Name = "terraform-const-soft-grupoB"
+  }
+}
+
+resource "aws_security_group" "allow_8080" {
+  name        = "allow_8080"
+  description = "Allow TCP 8080 inbound traffic"
+
+  ingress {
+    description = "TCP 8080 from anywhere"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_8080"
+  }
+}
