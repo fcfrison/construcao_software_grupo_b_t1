8cfd393adb80f5a35fa206d03115fc1b6a26e8d4
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-07-09 18:00:57 +0000

    bug: corrige bug no deploy.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -11,23 +11,3 @@ then
 else
     echo "Docker is already installed."
 fi
-
-# Navigate to the directory containing the Dockerfile
-# cd /home/ubuntu
-
-# Check if Dockerfile exists
-#if [ ! -f "Dockerfile" ]; then
-#    echo "Error: Dockerfile not found in $PWD"
-#    exit 1
-#fi
-
-# Build the Docker image
-sudo docker build -t const_softw:latest .
-
-# Check if the build was successful
-if [ $? -eq 0 ]; then
-    echo "Docker image built successfully"
-else
-    echo "Error: Docker image build failed"
-    exit 1
-fi
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+if [ ! -f "Dockerfile" ]; then
+    echo "Error: Dockerfile not found in $PWD"
+    exit 1
+fi
+
+# Build the Docker image
+sudo docker build -t const_softw:latest .
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Docker image built successfully"
+else
+    echo "Error: Docker image build failed"
+    exit 1
+fi
 sudo docker run -d --restart always --name const_soft_app -p 8080:8080 const_softw
