16e5df5a61b495fc6c08d7cf4d099bd06c55148c
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-07-09 18:24:46 +0000

    bug: corrige bug no deploy.

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-sudo docker kill const_soft_app
-sudo docker rm -v const_soft_app
+#!/bin/bash
+
+# Stop the running container
+if sudo docker ps -q --filter "name=const_softw_app" | grep -q .; then
+    echo "Stopping const_softw container"
+    sudo docker stop const_softw_app
+    sudo docker rm -v const_softw_app
+else
+    echo "No const_softw container running"
+fi
+
+echo "Application stopped"
