34b6cd34c0a4f546d37f499f272d2b887901e98f
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-07-09 21:59:16 +0000

    delete: deleta vahrias coisas.

--- src/main/java/com/grupoB/account/controllers/account/PasswordDTO.java
+++
@@ -1,8 +0,0 @@
-package com.grupoB.account.controllers.account;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record PasswordDTO(@JsonProperty("old_password") String oldPassword, 
-                          @JsonProperty("new_password") String newPassword, 
-                          @JsonProperty("new_password_confirmation") String newPasswordConfirmation) {
-}
