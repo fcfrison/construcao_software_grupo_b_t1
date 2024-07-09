f009014f9cf92238f5ab5a7eca4d9205c0902abc
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-07-09 19:07:00 +0000

    bug: corrige bug no deploy.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,7 @@ if [ ! -f "Dockerfile" ]; then
     exit 1
 fi
 # Stop the running container
-if sudo docker ps -q --filter "name=const_softw_app" | grep -q .; then
+if sudo docker ps -q --filter "name=const_soft_app" | grep -q .; then
     echo "Stopping const_soft_app container"
     sudo docker kill const_soft_app
     sudo docker rm -v const_soft_app
