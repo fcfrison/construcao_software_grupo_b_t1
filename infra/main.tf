5df4eff98b5eb5c7a46711446dafe560dba0eaaa
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-07-10 13:10:33 +0000

    bugfix: corrige bug no arquivo main.trf.

--- infra/main.tf
+++ infra/main.tf
@@ -142,8 +142,8 @@ resource "aws_instance" "example" {
               dpkg-deb -b codedeploy-agent_1.3.2-1902_ubuntu22/
               sudo dpkg -i codedeploy-agent_1.3.2-1902_ubuntu22.deb
               sudo systemctl list-units --type=service | grep codedeploy
-              sudo docker pull ${var.docker_account}/account:latest
-              docker run -d --name myapp -p 8080:8080 -e DATABASE_URL=jdbc:postgresql://${aws_db_instance.postgres.address}:5432/postgres ${var.docker_account}/account
+              sudo docker pull felipefrison/account:latest
+              docker run -d --name myapp -p 8080:8080 -e DATABASE_URL=jdbc:postgresql://${aws_db_instance.postgres.address}:5432/postgres felipefrison/account
               EOF
 
   tags = {
